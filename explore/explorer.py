31939c607a3fad304e2babbd6b64e7edb384de18
antleypk <peterantley@gmail.com>
2019-09-11 09:43:44 -0700

    explorer gets first "200"

---
+++ explore/explorer.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+
+# url = "https://openapi.etsy.com/v2/listings/active?limit=50&offset=0&api_key=bvpvd0ns8aqk63229f9baz9u"
+# headers = {'user-agent': 'my-app/0.0.1'}
+
+# r = requests.get(url, headers=headers)
+# print('r: {}'.format(r))
+# content = r.content
+# print('content: {}'.format(content))
+
