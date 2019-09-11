d7eb251d1cbc5e18b4a665f2608fdc6993cecb2a
antleypk <peterantley@gmail.com>
2019-09-11 08:31:38 -0700

    add the setup to repo

---
+++ bash_log.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#      a log of all the commands to do this
+
+
+#echo 'hello bash!'
+
+#mkdir take_home
+#cd take_home
+#touch bash_log.sh
+#chmod +x bash_log.sh
+
+#git clone https://github.com/antleypk/simon_data.git
+#cd simon_data
+#mkdir explore
+#cd explore
+
+cd ..
+cd ..
+
+
+
+git add .
+git commit -m 'add the setup to repo'
+git push
