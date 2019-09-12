3ebf94bd7f9448fc0ea12d1dc983cc312871dec8
antleypk <peterantley@gmail.com>
2019-09-12 15:02:37 -0400

    analyzer returns ids of shops to be analyzed

--- src/analyzer.py
+++ src/analyzer.py
@@ -11,22 +11,40 @@ def get_recent():
             dates.append(lcl_t)
 
     dates = sorted(dates, reverse=True)
-    date = dates[0]
-    return date
+    try:
+        date = dates[0]
+        return date
+    except IndexError:
+        print('Data Folder is missing shop lists') 
+        return 0
 
 def get_shops(timestamp):
     lcl_path = "./data/shops_{}.csv".format(timestamp)
-
+    shops = []
     with open(lcl_path, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        count = 0
         for row in reader:
-            print(row)
+            shop = {}
+            if count > 0:
+                shop['count'] = row[0]
+                shop['id'] = row[1]
+                shop['name'] = row[2]
+                shop['listings'] = row[3]
+                shops.append(shop)
+            count+=1
+    return shops
 
+def word_counter(shop_id):
+    pass
 
 def main():
     timestamp = get_recent()
-    get_shops(timestamp)
-
+    if not timestamp == 0:
+        shops = get_shops(timestamp)
+        for shop in shops:
+            print('shop: {}'.format(shop['id']))
+    else:
+        print("Please run 'scraper.py' first.")
 if __name__ == "__main__":
     main()
